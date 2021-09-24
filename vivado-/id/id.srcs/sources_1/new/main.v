316aab419c35624f22c5a710ec1882e306d6af1d
Bengt Marten Agren <marangisto@me.com>
2021-09-24 20:50:04 +0800

    Initiate tcl-based project export.

---
+++ vivado-/id/id.srcs/sources_1/new/main.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2021 04:46:40 PM
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main
+    ( input I
+    , output O
+    );
+
+    assign O = I;
+endmodule
