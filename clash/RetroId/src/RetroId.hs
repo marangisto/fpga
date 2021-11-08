bdd97bb75b127750a5dc5f9281b2770c67833a13
Bengt Marten Agren <marangisto@me.com>
2021-11-08 12:01:38 +0800

    Use correct code for this example.

--- clash/RetroId/src/RetroId.hs
+++ clash/RetroId/src/RetroId.hs
@@ -1,19 +1,11 @@
-module RetroId (topEntity, plus) where
+module RetroId where
 
 import Clash.Prelude
+import Clash.Annotations.TH
 
--- | Add two numbers. Example:
---
--- >>> plus 3 5
--- 8
-plus :: Signed 8 -> Signed 8 -> Signed 8
-plus a b = a + b
+topEntity
+    :: "BTN" ::: Signal System Bit
+    -> "LED" ::: Signal System Bit
+topEntity = id
 
--- | 'topEntity' is Clash's equivalent of 'main' in other programming
--- languages. Clash will look for it when compiling 'Example.Project'
--- and translate it to HDL. While polymorphism can be used freely in
--- Clash projects, a 'topEntity' must be monomorphic and must use non-
--- recursive types. Or, to put it hand-wavily, a 'topEntity' must be
--- translatable to a static number of wires.
-topEntity :: Signed 8 -> Signed 8 -> Signed 8
-topEntity = plus
+makeTopEntity 'topEntity
